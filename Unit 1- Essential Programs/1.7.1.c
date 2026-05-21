c2af8a33c2ffdd880429f2bd7ebedbf1c822126a
Khushi Bhakuni <khushi1bhakuni@gmail.com>
2026-05-22 02:15:49 +0530

    Sequence Generation

---
+++ Unit 1- Essential Programs/1.7.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0; i<n; i++){
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0; i<n; i++){
+		if(i==n-1){
+			break;
+		}
+		arr[i] = arr[i] + arr[i+1];
+	}
+	for(int i=0; i<n ; i++){
+		printf("%d ",arr[i]);
+	}
+}
