d3b2c408625519be85810f0b4678fbc67d9653d4
Khushi Bhakuni <khushi1bhakuni@gmail.com>
2026-05-22 03:04:27 +0530

    Sort the scores of athletes in descending order

---
+++ Unit 1- Essential Programs/1.7.5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	if(n<1){
+		printf("Invalid number\n");
+	}
+
+	else{
+		int arr[n];
+		for(int i=0; i<n; i++){
+			scanf("%d",&arr[i]);
+		}
+	
+		printf("Original Scores: ");
+	
+		for(int i=0; i<n; i++){
+			printf("%d ",arr[i]);
+		}
+	
+		for(int i=0; i<n-1; i++){
+			for(int j = i+1; j<n; j++){
+				if(arr[i]<arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	
+		printf("\nSorted Scores: ");
+	
+		for(int i=0; i<n; i++){
+			printf("%d ",arr[i]);
+		}
+		printf("\n");
+	}
+
+}
